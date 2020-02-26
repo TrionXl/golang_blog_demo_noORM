e78a572478da57cafe8ef6332b26ad50951166f1
YUANYUAN YU <yxd1069089904@gmail.com>
2020-02-26 21:13:59 +0800

    init

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"go_blog/service"
+)
+
+const servingAddress = ":1323"
+const (
+	authname = "admin"
+	authpw   = "secret"
+)
+
+func main() {
+	e := echo.New()
+	service.Router(e)                                                                          //启动路由
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) { //简单认证
+		if username == authname && password == authpw {
+			return true, nil
+		}
+		return false, nil
+	}))
+	e.Logger.Fatal(e.Start(servingAddress))
+}
